5faf5061e3c49e29b46faf27b18c39cd8549e95e
Ferdous-M <137258398+Ferdous-M@users.noreply.github.com>
2024-06-09 14:14:39 +0600

    Create optimal.cpp

---
+++ search-in-rotated-sorted-array-ii/optimal.cpp
@@ -0,0 +1,45 @@
+https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+TC: O(logN) , SC: (1);
+
+class Solution 
+{
+public:
+    int search(vector<int>& nums, int target) 
+    {
+        int low=0, high=nums.size()-1, mid;
+        while(low <= high)
+        {
+            mid = (low+high)>>1; //divide by 2 using  >> (right shift) , equavalent to (low+high)/2
+            if(nums[mid] == target)return true;
+            if(nums[low]==nums[mid]  && nums[mid]==nums[high]){
+                low++;high--;
+                continue;
+            }
+            if(nums[low] <= nums[mid])
+            {
+                if(nums[low]<=target && target<=nums[mid])
+                {
+                     high=mid-1;
+                }
+                else
+                {
+                    low=mid+1;
+                }
+            }
+            else
+            {
+              if(nums[mid]<=target && target<=nums[high])
+              {
+               low=mid+1;
+              }
+              else
+              {
+                high=mid-1;
+              }
+            }
+        }
+            return false;
+        
+    }
+
+};
