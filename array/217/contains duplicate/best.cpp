a74d24ebb8a051b9ab3367c7623461725cd9ad50
Ferdous <137258398+Ferdous-M@users.noreply.github.com>
2026-04-16 16:26:47 +0600

    Create best.cpp

---
+++ array/217/contains duplicate/best.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+
+        for (int num : nums) {
+            if (seen.count(num)) {
+                return true;
+            }
+            seen.insert(num);
+        }
+        return false;
+    }
+};
+
+// Time & Space Complexity
+// Time complexity: O(n)
+// Space complexity: O(n)
