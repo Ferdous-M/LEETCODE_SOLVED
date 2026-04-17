e1fa4ad9d0b9728d562f719d16100814b3404db4
Ferdous <137258398+Ferdous-M@users.noreply.github.com>
2026-04-17 12:18:21 +0600

    Add function to group anagrams
    Implement a function to group anagrams from a list of strings.

--- array/49/group anagrams.cpp
+++ array/49/group anagrams.cpp
@@ -1,1 +1,17 @@
 
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> res;
+        for (const auto& s : strs) {
+            string sortS = s;
+            sort(sortS.begin(), sortS.end());
+            res[sortS].push_back(s);
+        }
+        vector<vector<string>> result;
+        for (auto& pair : res) {
+            result.push_back(pair.second);
+        }
+        return result;
+    }
+};
